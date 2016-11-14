bccf0a490ab070d24be9309fdeb3f760a69c3409
cssmagic <cssmagic.cn@gmail.com>
2016-11-14 16:22:43 +0800

    Use `gulpfiles` to simplify building script.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@
 
 const path = require('path')
 const gulp = require('gulp')
-const del = require('del')
-const concat = require('gulp-concat')
+const gulpfiles = require('gulpfiles')
 const rename = require('gulp-rename')
 const wrap = require('gulp-wrap')
 const replace = require('gulp-replace')
-const uglify = require('gulp-uglify')
+
+const streamToPromise = require('gulp-stream-to-promise')
 
 const myPath = {
 	temp: './.tmp/',
@@ -15,61 +15,86 @@ const myPath = {
 	dest: './dist/',
 }
 const FILENAME = 'gearbox'
-const NS = 'gearbox'
-
-const scripts = [
-	'./src/core.js',
-	'./src/str.js',
-	'./src/root.js',
-	'./src/ua.js',
-	'./src/url.js',
-	'./src/dom.js',
-]
+// const NS = 'gearbox'
 
 const modules = {
 	action:   './bower_components/action/src/action.js',
 	template: './bower_components/underscore-template/src/underscore-template.js',
 }
 
-gulp.task('default', ['clean'], function () {
-	gulp.start('js')
+const scripts = {
+	[FILENAME + '.js']: [
+		'./src/core.js',
+		'./src/str.js',
+		'./src/root.js',
+		'./src/ua.js',
+		'./src/url.js',
+		'./src/dom.js',
+	]
+}
+// combine external modules
+Object.keys(modules).forEach(function (key) {
+	scripts[FILENAME + '.js'].push(path.join(myPath.temp, key + '.js'))
 })
 
-gulp.task('clean', function (callback) {
-	del(path.join(myPath.dest, '*.*'), callback)
-})
+gulp.task('clean', gulpfiles.del({
+	glob: path.join(myPath.dest, '*.*'),
+}))
 
-gulp.task('clean-temp', function (callback) {
-	del(path.join(myPath.temp, '*.*'), callback)
-})
+gulp.task('clean-temp', gulpfiles.del({
+	glob: path.join(myPath.temp, '*.*'),
+}))
 
-gulp.task('prepare-module', ['clean-temp'], function () {
+gulp.task('prepare-module', function () {
+	let tasks = []
 	Object.keys(modules).forEach(function (key) {
 		const src = modules[key]
-		gulp.src(src)
+		let stream = gulp.src(src)
 			.pipe(wrap('*/\n<%= contents %>\n/*'))
 			.pipe(wrap({src: path.join(myPath.src, '_wrapper/mod-' + key + '.js')}))
 			.pipe(rename(key + '.js'))
 			.pipe(gulp.dest(myPath.temp))
+		tasks.push(streamToPromise(stream))
 	})
+	return Promise.all(tasks)
 })
 
-gulp.task('js', function() {
-	// combine external modules
-	Object.keys(modules).forEach(function (key) {
-		scripts.push(path.join(myPath.temp, key + '.js'))
-	})
+gulp.task('js', gulpfiles.concat({
+	rules: scripts,
+	dest: myPath.dest,
+	config: {
+		pipes: [
+			{
+				plugin: 'wrap',
+				config: '*/\n<%= contents %>\n/*',
+			},
+			{
+				plugin: 'wrap',
+				config: {src: path.join(myPath.src, '_wrapper/dist-trad.js')},
+			},
+			{
+				plugin: 'replace',
+				config: [/\/\*\* DEBUG_INFO_START \*\*\//g, '/*'],
+			},
+			{
+				plugin: 'replace',
+				config: [/\/\*\* DEBUG_INFO_END \*\*\//g, '*/'],
+			},
+			{
+				plugin: 'uglify',
+				rename: FILENAME + '.min.js',
+				config: {
+					preserveComments: 'some',
+				},
+			}
+		]
+	},
+}))
 
-	gulp.src(scripts)
-		.pipe(concat(FILENAME + '.js'))
-		.pipe(wrap('*/\n<%= contents %>\n/*'))
-		.pipe(wrap({src: path.join(myPath.src, '_wrapper/dist-trad.js')}))
-		.pipe(replace(/\/\*\* DEBUG_INFO_START \*\*\//g, '/*'))
-		.pipe(replace(/\/\*\* DEBUG_INFO_END \*\*\//g, '*/'))
-		.pipe(gulp.dest(myPath.dest))
-		.pipe(uglify({
-			preserveComments: 'some'
-		}))
-		.pipe(rename(FILENAME + '.min.js'))
-		.pipe(gulp.dest(myPath.dest))
-})
+gulp.task('dist', gulp.series([
+	'clean',
+	'clean-temp',
+	'prepare-module',
+	'js',
+]))
+gulp.task('default', gulp.series('dist'))
